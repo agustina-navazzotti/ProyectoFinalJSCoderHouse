93d961715c468b0cdd8bc40a0c9997922bfbb8c3
Agus Navazzotti <marianavazzotti@gmail.com>
2022-04-30 16:32:49 -0300

    librerias y operadores

--- js/tareas.js
+++ js/tareas.js
@@ -7,40 +7,57 @@ class Tarea {
         this.responsable = responsable.toUpperCase();
         this.realizada = false;
     }
-
-    hacerTarea() {
-        this.realizada = true;
-    }
 }
 
-//declaración de arreglo para almacenar tareas pendientes
 
-var tareasPendientes = [];
+//declaración de arreglo para almacenar tareas pendientes
+const tareasPendientes = [];
+//localStorage.setItem("task", JSON.stringify(tareasPendientes)); 
+
+//almacenar tarea por tarea
+const guardarTareas = (c, v) => {localStorage.setItem(c, v)};
+
+guardarTareas("task", JSON.stringify(tareasPendientes)); 
 
 //añadir nueva tarea
-var botonTarea = document.getElementById("btnTask");
+let botonTarea = document.getElementById("btnTask");
 botonTarea.addEventListener("click", () => {
     let nom = document.getElementById("tarea").value;
     let prio = document.getElementById("prioridad").value;
     let res = document.getElementById("responsable").value;
-    tareasPendientes.push(new Tarea(nom, prio, res));
-    alert(`Tarea asignada: \n Nombre: ${nom} \n Prioridad: ${prio} \n Responsable: ${res}`);
-    localStorage.setItem("task", JSON.stringify(tareasPendientes));
+    let nueva = new Tarea(nom, prio, res);
+    tareasPendientes.push(nueva);
+    Swal.fire({
+        icon: 'success',
+        title: '¡MIAU!',
+        text: 'Tu tarea se cargó correctamente',
+        showConfirmButton: false,
+        timer: 3000
+      });
+    guardarTareas("task", JSON.stringify(tareasPendientes)); 
 });
+
+
 
 //marcar tarea como realizada
 
 let tareaCompleta = document.getElementById("tareaRealizada");
 tareaCompleta.addEventListener("click", () => {
     let tar = (prompt("Escribe la tarea realizada: \n")).toUpperCase();
-    let taskSec =JSON.parse(localStorage.getItem(`task`));
+    let taskSec =JSON.parse(localStorage.getItem('task'));
     let i;
     taskSec.forEach(Tarea =>{
         Tarea.nombre === tar && (i= taskSec.indexOf(Tarea));
         }
     );
-    tareasPendientes[i].hacerTarea(); 
-    alert(`Tarea completa`);
+    tareasPendientes[i].realizada = true; 
+    Swal.fire({
+        icon: 'success',
+        title: '¡Qué rico!',
+        text: 'Completaste tu tarea correctamente y alimentaste a tu michi virtual',
+        showConfirmButton: false,
+        timer: 3000
+      });
     localStorage.setItem("task", JSON.stringify(tareasPendientes));
 })
 
