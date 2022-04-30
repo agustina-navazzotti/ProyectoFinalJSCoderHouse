1f714dfc05de89c6b6f21575e92529e8b4dd8e97
Agus Navazzotti <marianavazzotti@gmail.com>
2022-04-30 02:26:19 -0300

    salvando errores de localStorage

--- js/tareas.js
+++ js/tareas.js
@@ -9,7 +9,7 @@ class Tarea {
     }
 
     hacerTarea() {
-        this.realizado = true;
+        this.realizada = true;
     }
 }
 
@@ -18,7 +18,7 @@ class Tarea {
 var tareasPendientes = [];
 
 //añadir nueva tarea
-let botonTarea = document.getElementById("btnTask");
+var botonTarea = document.getElementById("btnTask");
 botonTarea.addEventListener("click", () => {
     let nom = document.getElementById("tarea").value;
     let prio = document.getElementById("prioridad").value;
@@ -26,20 +26,35 @@ botonTarea.addEventListener("click", () => {
     tareasPendientes.push(new Tarea(nom, prio, res));
     alert(`Tarea asignada: \n Nombre: ${nom} \n Prioridad: ${prio} \n Responsable: ${res}`);
     localStorage.setItem("task", JSON.stringify(tareasPendientes));
-})
+});
 
 //marcar tarea como realizada
 
 let tareaCompleta = document.getElementById("tareaRealizada");
 tareaCompleta.addEventListener("click", () => {
-    let tar = prompt("Escribe la tarea realizada: \n");
-    let i = localStorage.getItem(JSON.parse(tareasPendientes).indexOf(tar.toUpperCase));
-    tareasPendientes[i].hacerTarea();
+    let tar = (prompt("Escribe la tarea realizada: \n")).toUpperCase();
+    let taskSec =JSON.parse(localStorage.getItem(`task`));
+    let i;
+    taskSec.forEach(Tarea =>{
+        Tarea.nombre === tar && (i= taskSec.indexOf(Tarea));
+        }
+    );
+    tareasPendientes[i].hacerTarea(); 
     alert(`Tarea completa`);
-    localStorage.setItem("task", JSON.stringify(tareas));
+    localStorage.setItem("task", JSON.stringify(tareasPendientes));
 })
 
-/* var opcion = parseInt(prompt("MENÚ (Elegí una opción para realizar una acción: \n 1. Agregar tarea pendiente \n 2. Marcar tarea como realizada \n 3. Filtrar tareas urgentes y prioritarias \n 4.Mostrar lista de tareas pendientes \n 5. Mostrar tareas que tengan de responsable a \n 6. Imprimir  lista de tareas completa (responsables, prioridad y estado) \n 7. Para salir \n"));
+/*marcar tarea como realizada
+
+let tareasPrioritarias = document.getElementById("tareasPrio");
+tareasPrioritarias.addEventListener("click", () => {
+    let taskPrio = JSON.parse(localStorage.getItem(`task`));
+    taskSec.forEach(el =>{
+        el.prioridad >= 4 && alert(`${el.nombre}`);
+        }
+})
+
+var opcion = parseInt(prompt("MENÚ (Elegí una opción para realizar una acción: \n 1. Agregar tarea pendiente \n 2. Marcar tarea como realizada \n 3. Filtrar tareas urgentes y prioritarias \n 4.Mostrar lista de tareas pendientes \n 5. Mostrar tareas que tengan de responsable a \n 6. Imprimir  lista de tareas completa (responsables, prioridad y estado) \n 7. Para salir \n"));
 
 while (opcion != 7){
     switch(opcion){
